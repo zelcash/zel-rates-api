692bea1507adfba8509c1858eb7f97fc24a8da77
TheTrunk <TheTrunk@users.noreply.github.com>
2018-03-05 19:52:04 +0100

    Switching to Graviex

--- src/services/zelRates.js
+++ src/services/zelRates.js
@@ -3,12 +3,12 @@ const request = require('request-promise-native');
 const zelRates = {
   getAll() {
     return Promise.all([
-      request({ uri: 'https://stocks.exchange/api2/ticker', json: true }),
+      request({ uri: 'https://graviex.net/api/v2/tickers/zelbtc', json: true, strictSSL: false }),
       request({ uri: 'https://bitpay.com/api/rates', json: true }),
     ]).then((results) => {
       const cmcData = results[0]; // results from stock exchange
       const bitpayData = results[1]; // results from bitpay
-      const zelBtcExchangeRate = cmcData[181].last;
+      const zelBtcExchangeRate = cmcData.ticker.last;
       const rates = [];
 
       bitpayData.forEach((value) => {
